88ff18da4e649583d6b062061d70c31c0082fead
SawaKrasava <159466840+SawaKrasava@users.noreply.github.com>
2024-10-24 19:18:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee_machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
